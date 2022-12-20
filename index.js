3ed4cfe0556d6815db35a1190532918b6ca766e8
ddoyediran <ddoyediran@gmail.com>
2022-12-20 08:11:17 +0100

    more commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -8,6 +9,7 @@ morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
 });
 
+app.use("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
