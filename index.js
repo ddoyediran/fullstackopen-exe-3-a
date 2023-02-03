571183940bd6afbeb2b659523763d95d0e4fbbd5
ddoyediran <ddoyediran@gmail.com>
2023-02-03 19:03:35 +0100

    more commit

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const Phonebook = require("./models/phoneAddress");
 const { response } = require("express");
 require("dotenv").config();
-// const { request, response } = require("express");
 
 const app = express();
 
@@ -37,53 +36,24 @@ app.use(morgan(":method :url :body")); // log post method, url and data
 
 const PORT = process.env.PORT || 3001;
 
-let phonebook = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 // GET method: fetches all the list in the phonebook
 app.get("/api/persons", (req, res) => {
   Phonebook.find({}).then((phonebook) => {
     res.json(phonebook);
   });
-
-  // if (!phonebook) {
-  //   return res.status(404).json({ message: "phonebook is empty" });
-  // }
-
-  // return res.status(200).send(phonebook);
 });
 
 // GET method: fetches number of entries in the phonebook
-app.get("/info", (req, res) => {
-  const numberOfEntries = phonebook.length;
+// app.get("/info", (req, res) => {
+//   const numberOfEntries = phonebook.length;
 
-  const currentDate = new Date();
-  console.log(`Phone book has info for ${numberOfEntries} people`);
-  console.log(currentDate);
-  //res.send(`Phone book has info for ${numberOfEntries} people`);
-  //res.send(currentDate);
-  return;
-});
+//   const currentDate = new Date();
+//   console.log(`Phone book has info for ${numberOfEntries} people`);
+//   console.log(currentDate);
+//   //res.send(`Phone book has info for ${numberOfEntries} people`);
+//   //res.send(currentDate);
+//   return;
+// });
 
 // GET: fetch a single address from the phonebook
 app.get("/api/persons/:id", (req, res, next) => {
@@ -97,18 +67,6 @@ app.get("/api/persons/:id", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-// app.get("/api/persons/:id", (req, res) => {
-//   const person = phonebook.find((personDetails) => {
-//     return personDetails.id === parseInt(req.params.id);
-//   });
-
-//   if (!person) {
-//     return res.status(404).json({ message: "Person not found in the list" });
-//   }
-
-//   return res.status(200).send(person);
-// });
-
 // DELETE: delete an address from the phonebook
 app.delete("/api/persons/:id", (req, res, next) => {
   Phonebook.findByIdAndRemove(req.params.id)
@@ -116,19 +74,6 @@ app.delete("/api/persons/:id", (req, res, next) => {
       res.status(204).end();
     })
     .catch((error) => next(error));
-  //   phonebook = phonebook.filter((phonebook) => {
-  //     return phonebook.id != parseInt(req.params.id);
-  //   });
-  //   return res.status(204).end();
-  // Alternative solution
-  // const personIndex = phonebook.findIndex((phonebook) => {
-  //   return phonebook.id === parseInt(req.params.id);
-  // });
-  // if (personIndex < 0) {
-  //   return res.status(404).send("Address note found!");
-  // }
-  // phonebook.splice(personIndex, 1);
-  // return res.status(204).end();
 });
 
 // POST: add new address to the phonebook
@@ -149,29 +94,7 @@ app.post("/api/persons", (req, res) => {
   });
 });
 
-// app.post("/api/persons/", (req, res) => {
-//   // check if number and name are not empty
-//   if (!req.body.name || !req.body.number) {
-//     return res.status(404).send("Name or number is missing");
-//   }
-
-//   for (let i = 0; i < phonebook.length; i++) {
-//     if (req.body.name === phonebook[i].name) {
-//       return res.status(404).json({ error: "name must be unique" });
-//     }
-//   }
-
-//   const newPhoneAddress = {
-//     id: generateId(),
-//     name: req.body.name,
-//     number: req.body.number,
-//   };
-
-//   phonebook = phonebook.concat(newPhoneAddress);
-
-//   return res.status(200).send("Address added successfully!");
-// });
-
+// PUT: update address in the phonebook
 app.put("/api/persons/:id", (req, res, next) => {
   const body = req.body;
 
